04aad0cf7a1fdd26cb40e28d31c0298c665ef925
sishirsubedi <subedisishir@gmail.com>
2024-02-12 23:29:20 -0800

    data load module

--- picasa/dutil/data.py
+++ picasa/dutil/data.py
@@ -5,6 +5,9 @@ Base classes for data definition
 from typing import List, Mapping
 from ..util.typehint import Adata
 
+import numpy as np
+from scipy.sparse import csr_matrix 
+
 
 def configure_dataset(
     adata : Adata
@@ -16,3 +19,23 @@ class Dataset:
     def __init__(self, adata_list : Mapping[str, Adata]) -> None:
         self.adata_list = adata_list
             
+
+
+def load_data(adata, start_index: int, end_index: int)-> np.array:
+    data = adata.X.data
+    indices = adata.X.indices
+    indptr = adata.X.indptr
+    shape = adata.X.shape
+    selected_gene_indices = adata.uns['selected_genes']
+    
+    mtx = []
+    for ci in range(start_index,end_index,1):
+        mtx.append(np.asarray(
+        csr_matrix((data[indptr[ci]:indptr[ci+1]], 
+        indices[indptr[ci]:indptr[ci+1]], 
+        np.array([0,len(indices[indptr[ci]:indptr[ci+1]])])), 
+        shape=(1,shape[1])).todense()).flatten())
+    
+    mtx = np.asarray(mtx)
+    mtx = mtx[:,selected_gene_indices]
+    return mtx
--- prep.py
+++ prep.py
@@ -7,5 +7,15 @@ spatial = ad.read_h5ad('data/brca_spatial.h5ad')
 
 pico = picasa.create_picasa_object({'rna':rna,'spatial':spatial})
 
-
 pico.update_common_features()
+
+import numpy as np
+mtx = np.random.random((200,100))
+
+depth = 3
+ndims = 200
+replicates = 2
+
+rp_arrs = projection_matrix(depth,ndims,replicates)
+
+z = get_projection(mtx,rp_arrs,rp_weight_adjust=False,ndim=10)
